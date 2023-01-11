333511d9431abcecc448e8c9a24c58f7bde6f29c
manhualu <manhua.lu64@gmail.com>
2023-01-11 16:17:10 +1100

    Completed q1-frontend part 1

--- q1-frontend/components/Table/Table.tsx
+++ q1-frontend/components/Table/Table.tsx
@@ -45,7 +45,7 @@ export default function Table() {
       },
       {
         alert: 'Done!',
-        status: '<YOUR NAME>',
+        status: 'manhualu',
         updates: []
       }
     ]
@@ -67,7 +67,12 @@ export default function Table() {
               {content.status}
             </div>
             <div className={styles.item}>
-              {/* TODO: add updates */}
+              {content.updates.map((update) => 
+              <div className={styles.updates}>
+                <div>{update.update}</div>
+                <div className={styles.updatesDate}>{update.date}</div>  
+                </div>
+              )}
             </div>
           </div>
         ))}
